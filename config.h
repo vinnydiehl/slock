f0a027af4d1c1056540ce7cc8f9671ee7a6e5fba
Vinny Diehl <vinny.diehl@gmail.com>
2023-10-03 02:20:54 -0700

    Set zenburn color

--- config.h
+++ config.h
@@ -4,8 +4,8 @@ static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "#000000",   /* after initialization */
-	[INPUT] =  "#282c34",   /* during input */
-	[FAILED] = "#be5046",   /* wrong password */
+	[INPUT] =  "#3f3f3f",   /* during input */
+	[FAILED] = "#3f3f3f",   /* wrong password */
 };
 
 /* lock screen opacity */
@@ -15,7 +15,7 @@ static const float alpha = 0.5;
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "Enter password to unlock";
+static const char * message = "";
 
 /* text color */
 static const char * text_color = "#abb2bf";
